c68a8aa5faef1a9afa6dd24d20d4516a01cadcf1
nicko <ikar2021@hotmail.com>
2022-05-30 16:23:15 +0300

    add field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -1,6 +1,8 @@
+from email.policy import default
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
@@ -52,7 +54,9 @@ class Flat(models.Model):
         User,
         verbose_name='Кто лайкнул:',
         related_name='liked_flats',
+        blank=True,
         )
+    owner_pure_phone = PhoneNumberField(null=True, blank=True, default=None)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
